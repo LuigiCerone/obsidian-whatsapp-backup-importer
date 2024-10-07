dda1032f990bc318d3b1f8ae07d97cda559d1faa
LuigiCerone <luigicerone96@gmail.com>
2024-10-07 21:54:52 +0200

    Fix zip reading

--- src/utils/zip.ts
+++ src/utils/zip.ts
@@ -1,8 +1,15 @@
-import { BlobReader, ZipReader } from '@zip.js/zip.js';
+import { promises as fs } from 'fs';
+import { BlobReader, Entry, ZipReader } from '@zip.js/zip.js';
 
-export async function readZip(file: string): Promise<void> {
-	console.log('ehere')
-    const reader = new ZipReader(new BlobReader(new Blob([file])));
-    const entries = await reader.getEntries({ filenameEncoding: 'utf-8' });
-    console.log(entries); // or handle entries as needed
+export async function readZip(path: string): Promise<Entry[]> {
+
+    // Read the file from the filesystem as a Buffer
+    const buffer = await fs.readFile(path);
+
+    // Convert the Buffer to a Blob (which zip.js understands)
+    const blob = new Blob([buffer]);
+
+    const reader = new ZipReader(new BlobReader(blob));
+
+    return await reader.getEntries();
 }
