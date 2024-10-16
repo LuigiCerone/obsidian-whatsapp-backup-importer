3cf485afdef4a4fdbd31e4a8ee3055e8da4d4768
LuigiCerone <luigicerone96@gmail.com>
2024-10-16 21:00:00 +0200

    Add chat file format logic

--- src/Importer.ts
+++ src/Importer.ts
@@ -1,8 +1,19 @@
-import { App, Notice, TFolder, Vault } from "obsidian";
+import { App, FileSystemAdapter, Notice, TFolder, Vault } from "obsidian";
 import { CustomZipEntry, readZip } from "./utils/zip";
+
+import * as fs from 'fs';
+import * as whatsapp from 'whatsapp-chat-parser';
 
 const IMAGE_FOLDER_NAME: string = "images"
 const VIDEO_FOLDER_NAME: string = "videos"
+const CHAT_FILE_NAME: string = "chats.md";
+
+type Message = {
+	author: string;
+	date: Date;
+	message: string;
+	attachment?: { fileName: string };
+}
 
 export class Importer {
     app: App;
@@ -20,6 +31,8 @@ export class Importer {
     }
 
     async run() {
+		let { vault } = this.app;
+
         this.outputFolder = await this.getOutputFolder();
 
 		if (!this.outputFolder) {
@@ -30,6 +43,11 @@ export class Importer {
         const zipEntries = await readZip(this.inputArchivePath)
 
 		await this.copyDataInVault(zipEntries);
+		const formattedFile: string = await this.processChatFile();
+		console.log(formattedFile);
+
+		const path = `${this.outputFolder?.path}/clean_${CHAT_FILE_NAME}`;
+		await vault.create(path, formattedFile);
     }
 
     async getOutputFolder(): Promise<TFolder | null> {
@@ -73,8 +91,8 @@ export class Importer {
 		for (let entry of entries) {
 			let path: string = "";
 	
-			if (entry.path.endsWith('_chat.txt')) {
-				path = `${this.outputFolder?.path}/chats.md`;
+			if (entry.path.endsWith('txt') && entry.path.toLowerCase().includes('chat')) {
+				path = `${this.outputFolder?.path}/${CHAT_FILE_NAME}`;
 				await vault.create(path, entry.content as string);
 	
 			} else if (entry.path.endsWith('jpg') || entry.path.endsWith('png')) {
@@ -86,5 +104,46 @@ export class Importer {
 					await vault.createBinary(path, entry.content as Buffer);
 			}	
 		}
+	}
+
+	formatMessageToMarkdown(msg: Message): string {
+		const formattedDate = msg.date.toLocaleString('it-IT', {
+			weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit'
+		  });
+	  
+		  // If the message contains the attachment filename, remove it from the message text
+		  let messageText = msg.message;
+		  if (msg.attachment && msg.message.includes(msg.attachment.fileName)) {
+			messageText = messageText.replace(msg.attachment.fileName, '').trim();
+		  }
+	  
+		  let markdown = `### ${msg.author} - ${formattedDate}\n\n`;
+		  markdown += `${messageText}\n`;
+	  
+		  // If there's an attachment, create a link to it if it's a jpg or mp4
+		  if (msg.attachment) {
+			const fileName = msg.attachment.fileName;
+			if (fileName.endsWith('.jpg')) {
+			  markdown += `\n**Attachment:** ![${fileName}](${this.outputFolder?.path}/${IMAGE_FOLDER_NAME}/${fileName})\n`;
+			} else if (fileName.endsWith('.mp4')) {
+				markdown += `\n**Attachment:** ![${fileName}](${this.outputFolder?.path}/${VIDEO_FOLDER_NAME}/${fileName})\n`;
+			} else {
+			  markdown += `\n**Attachment:** ${fileName}\n`;
+			}
+		  }
+	  
+		  markdown += '\n---\n';  // Add a separator between messages
+		  return markdown;
+	}
+
+	async processChatFile(): Promise<string> {
+		const adapter = this.app.vault.adapter as FileSystemAdapter;
+
+		const text = fs.readFileSync(`${adapter.getBasePath()}/${this.outputFolder?.path}/${CHAT_FILE_NAME}`, 'utf8');
+		const messages = whatsapp.parseString(text, {
+			parseAttachments: true
+		}) as Message[];
+
+		return messages.map(msg => this.formatMessageToMarkdown(msg)).join('');
 	}
 }
